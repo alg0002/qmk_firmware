3e87f510036d5db6a2ac4d3220df0f2c40c3c52c
alg <alg.info@gmail.com>
2022-12-29 21:14:36 +0900

    Modify split,matrix

--- keyboards/alg0002/lets_swamp/keymaps/default/keymap.c
+++ keyboards/alg0002/lets_swamp/keymaps/default/keymap.c
@@ -2,20 +2,20 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
-     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     * ┌───┬───┬───┬───┬───┬───┐    ┌───┬───┬───┬───┬───┬───┐
+     * │Esc│ Q │ W │ E │ R │ T │    │ Y │ U │ I │ O │ P │ @ │
+     * ├───┼───┼───┼───┼───┼───┤    ├───┼───┼───┼───┼───┼───┤
+     * │ - │ A │ S │ D │ F │ G │    │ H │ J │ K │ L │ ; │ : │
+     * ├───┼───┼───┼───┼───┼───┤    ├───┼───┼───┼───┼───┼───┤
+     * │Tab│ Z │ X │ C │ V │ B │    │ N │ M │ , │ . │ / │ \ │
+     * ├───┼───┼───┼───┼───┼───┤    ├───┼───┼───┼───┼───┼───┤
+     * │E/J│GUI│Alt│Sft│Ctl│Spc│    │Spc│Ctl│Sft│Alt│GUI│App│
+     * └───┴───┴───┴───┴───┴───┘    └───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x12(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L
+    [0] = LAYOUT(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_AT,
+        KC_MINS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_INT1,
+        KC_GRV , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_SPC,           KC_SPC,  KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, KC_APP
     )
 };
