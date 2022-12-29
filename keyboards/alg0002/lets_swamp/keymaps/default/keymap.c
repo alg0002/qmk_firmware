fd3e61f959ca2451ce2e1083addceea853d2852a
alg <alg.info@gmail.com>
2022-12-29 21:28:20 +0900

    Add rgblight ...  but no light

--- keyboards/alg0002/lets_swamp/keymaps/default/keymap.c
+++ keyboards/alg0002/lets_swamp/keymaps/default/keymap.c
@@ -13,9 +13,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┴───┴───┴───┘    └───┴───┴───┴───┴───┴───┘
      */
     [0] = LAYOUT(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_AT,
+        RGB_TOG, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_AT,
         KC_MINS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_INT1,
-        KC_GRV , KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_SPC,           KC_SPC,  KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, KC_APP
+        KC_GRV,  KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_SPC,           KC_SPC,  KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, KC_APP
     )
 };
