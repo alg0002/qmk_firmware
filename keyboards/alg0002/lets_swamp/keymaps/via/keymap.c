93049032b7486d805361bcdc4e0f5fc0aba0e62a
alg <alg.info@gmail.com>
2022-12-30 14:13:50 +0900

    Add mod tap keys

--- keyboards/alg0002/lets_swamp/keymaps/via/keymap.c
+++ keyboards/alg0002/lets_swamp/keymaps/via/keymap.c
@@ -1,5 +1,13 @@
 #include QMK_KEYBOARD_H
 
+#define L1_SPC  LT(1,KC_SPC)
+#define L2_ZKHK LT(2,KC_GRV)
+#define L2_APP  LT(2,KC_APP)
+#define MU_LSFT LSFT_T(KC_MHEN)
+#define ET_LCTL LCTL_T(KC_ENT)
+#define ET_RCTL RCTL_T(KC_ENT)
+#define HE_RSFT RSFT_T(KC_HENK)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┐    ┌───┬───┬───┬───┬───┬───┐
@@ -16,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_AT,
         KC_MINS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_INT1,
-        LT(2,KC_GRV),KC_LGUI,KC_LALT,KC_LSFT,KC_LCTL,LT(1,KC_SPC), LT(1,KC_SPC),KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, LT(2,KC_APP)
+        L2_ZKHK, KC_LGUI, KC_LALT, MU_LSFT, ET_LCTL, L1_SPC,           L1_SPC,  ET_RCTL, HE_RSFT, KC_RALT, KC_RGUI, L2_APP
     ),
 
     [1] = LAYOUT(
