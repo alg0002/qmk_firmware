f42bfafba69ab56e004e53773e871d891ccd353a
alg <alg.info@gmail.com>
2023-04-13 10:53:54 +0900

    Change LED order

--- keyboards/alg0002/lets_swamp/config.h
+++ keyboards/alg0002/lets_swamp/config.h
@@ -20,4 +20,4 @@
 //#define NO_ACTION_ONESHOT
 
 #define RGBLED_NUM 12
-#define RGBLIGHT_LED_MAP { 0, 1, 2, 3, 4, 5,11,10, 9, 8, 7, 6 }
+#define RGBLIGHT_LED_MAP { 5, 4, 3, 2, 1, 0, 6, 7, 8, 9,10,11 }
