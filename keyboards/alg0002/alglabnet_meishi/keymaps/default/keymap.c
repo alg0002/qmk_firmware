13c98eee676f2d82baf1bb2754938c8f074ff4ae
alg <alg.info@gmail.com>
2023-10-31 17:07:28 +0900

    [alg0002/Alglabnet-Meishi] Modify default

--- keyboards/alg0002/alglabnet_meishi/keymaps/default/keymap.c
+++ keyboards/alg0002/alglabnet_meishi/keymaps/default/keymap.c
@@ -5,14 +5,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┐
-     * │ A │ B │ C │
-     * ├───┼───┼───┤
-     * │ D │ E │ F │
-     * └───┴───┴───┘
+     * ┌───┬───┬───┬───┐
+     * │ A │ B │ C │ D │
+     * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_2x3(
-        KC_A,    KC_B,    KC_C,
-        KC_D,    KC_E,    KC_F
+    [0] = LAYOUT(
+        KC_A,    KC_B,    KC_C,    KC_D
     )
 };
